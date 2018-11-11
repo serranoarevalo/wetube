5c03370a7f5f79871a1cec26d08fed2899912a32
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2018-11-11 16:20:05 +0900

    Fullscreen

--- assets/js/videoPlayer.js
+++ assets/js/videoPlayer.js
@@ -1,11 +1,19 @@
 import getBlobDuration from "get-blob-duration";
 const videoContainer = document.querySelector(".video__container");
-let videoPlayer, playBtn, volumeBtn, currentTime, timerInterval, totalTime;
+let videoPlayer,
+  playBtn,
+  volumeBtn,
+  currentTime,
+  timerInterval,
+  totalTime,
+  fullScreenBtn;
 
 const PLAY_ICON = `<i class="fas fa-play"></i>`;
 const PAUSE_ICON = `<i class="fas fa-pause"></i>`;
 const VOLUME_MEDIUM = `<i class="fas fa-volume-down"></i>`;
 const VOLUME_MUTED = `<i class="fas fa-volume-off"></i>`;
+const FULLSCREEN = `<i class="fas fa-expand"></i>`;
+const FULLSCREEN_OUT = `<i class="fas fa-compress"></i>`;
 
 const formatDate = seconds => {
   var sec_num = parseInt(seconds, 10); // don't forget the second param
@@ -72,11 +80,44 @@ const getDuration = async () => {
   totalTime.innerHTML = formatDate(Math.floor(duration));
 };
 
+const exitFullScreen = () => {
+  fullscreenIcon();
+  if (document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if (document.mozCancelFullScreen) {
+    document.mozCancelFullScreen();
+  } else if (document.webkitExitFullscreen) {
+    document.webkitExitFullscreen();
+  } else if (document.msExitFullscreen) {
+    document.msExitFullscreen();
+  }
+  fullScreenBtn.removeEventListener("click", exitFullScreen);
+  fullScreenBtn.addEventListener("click", enterFullScreen);
+};
+
+const enterFullScreen = () => {
+  if (videoContainer.requestFullscreen) {
+    videoContainer.requestFullscreen();
+  } else if (videoContainer.mozRequestFullScreen) {
+    videoContainer.mozRequestFullScreen();
+  } else if (videoContainer.webkitRequestFullscreen) {
+    videoContainer.webkitRequestFullscreen();
+  } else if (videoContainer.msRequestFullscreen) {
+    videoContainer.msRequestFullscreen();
+  }
+  outFullScreenIcon();
+  fullScreenBtn.removeEventListener("click", enterFullScreen);
+  fullScreenBtn.addEventListener("click", exitFullScreen);
+};
+
 const volumeMuteIcon = () => (volumeBtn.innerHTML = VOLUME_MUTED);
 const volumeMediumIcon = () => (volumeBtn.innerHTML = VOLUME_MEDIUM);
 
 const playerPlayIcon = () => (playBtn.innerHTML = PLAY_ICON);
 const playerPauseIcon = () => (playBtn.innerHTML = PAUSE_ICON);
+
+const fullscreenIcon = () => (fullScreenBtn.innerHTML = FULLSCREEN);
+const outFullScreenIcon = () => (fullScreenBtn.innerHTML = FULLSCREEN_OUT);
 
 const initVideoPlayer = () => {
   videoPlayer = videoContainer.querySelector(".video__player");
@@ -84,10 +125,12 @@ const initVideoPlayer = () => {
   volumeBtn = videoContainer.querySelector("#js-volume");
   currentTime = videoContainer.querySelector("#video__current-time");
   totalTime = videoContainer.querySelector("#video__total-time");
+  fullScreenBtn = videoContainer.querySelector("#js-fullscreen");
   getDuration();
   playBtn.addEventListener("click", handlePlayBtn);
   volumeBtn.addEventListener("click", handleVolumeClick);
   videoPlayer.addEventListener("ended", handleEnded);
+  fullScreenBtn.addEventListener("click", enterFullScreen);
   videoPlayer.volume = 0.5;
 };
 
